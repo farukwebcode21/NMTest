c3199c56010df43ccbed9693d17a58e68b9e3c07
farukwebcode21 <farukahmadmk@yahoo.com>
2021-07-01 22:59:09 +0600

    Lesson To fs Module Example

--- Lesson2/index.js
+++ Lesson2/index.js
@@ -1,47 +1,47 @@
 //How to Write File ========================
 
-// const fs = require('fs');
-// fs.writeFile('demo.text', 'My Name is Faruk Ahmad ',(err) =>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         console.log('successfull');
-//     }
-// })
+const fs = require('fs');
+fs.writeFile('demo.text', 'My Name is Faruk Ahmad ',(err) =>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('successfull');
+    }
+})
 
 //How to appendFile ========================
 
-// const fs = require('fs');
-// fs.appendFile('demo.text', 'My Name is Faruk Ahmad and My Brother Name is jueal ',(err) =>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         console.log('successfull');
-//     }
-// })
+const fs = require('fs');
+fs.appendFile('demo.text', 'My Name is Faruk Ahmad and My Brother Name is jueal ',(err) =>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('successfull');
+    }
+})
 
 
 // How to readFile ========================
 
-// const fs = require('fs');
-// fs.readFile('demo.text', 'utf-8', (err, data) =>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         console.log(data);
-//     }
-// });
+const fs = require('fs');
+fs.readFile('demo.text', 'utf-8', (err, data) =>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log(data);
+    }
+});
 
 // How to rename ========================
 
-// const fs = require('fs');
-// fs.rename('demo.text', 'demo2.text', (err)=>{
-//     if(err){
-//         console.log(err)
-//     }else{
-//         console.log('Success-full')
-//     }
-// });
+const fs = require('fs');
+fs.rename('demo.text', 'demo2.text', (err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('Success-full')
+    }
+});
 
 // How to unlink / Delete  ========================
 
